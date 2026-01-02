e0949287ee56a765599d3003db1eb93ba875e2e7
Lukas5937 <lukas-reich@t-online.de>
2026-01-02 12:06:43 +0100

    Set up Home route

--- frontend/morning-grind/src/App.jsx
+++ frontend/morning-grind/src/App.jsx
@@ -1,7 +1,14 @@
-import './App.css'
+import "./App.css";
+import { Outlet } from "react-router";
 
 function App() {
-  return <></>
+  return (
+    <>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
