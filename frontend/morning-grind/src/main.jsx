df5a05ccc0557441580446b1a8a558d95f929595
Lukas5937 <lukas-reich@t-online.de>
2026-01-03 11:53:39 +0100

    Create /catalogue route

--- frontend/morning-grind/src/main.jsx
+++ frontend/morning-grind/src/main.jsx
@@ -4,12 +4,16 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import App from "./App.jsx";
 import Home from "./pages/home/Home.jsx";
+import Catalogue from "./pages/catalogue/Catalogue.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [{ path: "/", element: <Home /> }],
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/catalogue", element: <Catalogue /> },
+    ],
   },
 ]);
 
