1e0a469ccb4f614ad1f755917909824832445624
Lukas5937 <lukas-reich@t-online.de>
2026-01-03 11:53:55 +0100

    Add router links to navbar

--- frontend/morning-grind/src/components/Navbar.jsx
+++ frontend/morning-grind/src/components/Navbar.jsx
@@ -1,25 +1,29 @@
+import { Link } from "react-router";
+
 export default function Navbar() {
   return (
     <nav className="flex items-center justify-between px-8 py-6">
       <h1 className="text-2xl font-semibold">Morning Grind</h1>
+
       <div className="font-body flex gap-8">
-        <a href="#" className="text-gray-700 hover:text-black">
+        <Link to="/" className="text-gray-700 hover:text-black">
           Home
-        </a>
-        <a href="#" className="text-gray-700 hover:text-black">
+        </Link>
+        <Link to="/catalogue" className="text-gray-700 hover:text-black">
           Catalogue
-        </a>
-        <a href="#" className="text-gray-700 hover:text-black">
+        </Link>
+        <Link to="/" className="text-gray-700 hover:text-black">
           Search
-        </a>
+        </Link>
       </div>
+
       <div className="font-body flex gap-6">
-        <a href="#" className="text-gray-700 hover:text-black">
+        <Link to="/" className="text-gray-700 hover:text-black">
           Login
-        </a>
-        <a href="#" className="text-gray-700 hover:text-black">
+        </Link>
+        <Link to="/" className="text-gray-700 hover:text-black">
           Cart (0)
-        </a>
+        </Link>
       </div>
     </nav>
   );
