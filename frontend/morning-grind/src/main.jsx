1722ccc88a78c9901d6e1f9fe26dbd5190739e31
Lukas5937 <lukas-reich@t-online.de>
2026-01-03 08:32:54 +0100

    Create home page

--- frontend/morning-grind/src/main.jsx
+++ frontend/morning-grind/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import App from "./App.jsx";
-import Home from "./pages/Home.jsx";
+import Home from "./pages/home/Home.jsx";
 
 const router = createBrowserRouter([
   {
---
+++ frontend/morning-grind/src/pages/home/sections/Message.jsx
@@ -0,0 +1,9 @@
+export default function Message() {
+  return (
+    <section className="py-48 text-center">
+      <h3 className="text-5xl text-gray-800">
+        Wake up. Drink coffee. Be awesome.
+      </h3>
+    </section>
+  );
+}
