995848bfd219f8aadecfa3b4e7ea2e7fe8efc4a5
Lukas5937 <lukas-reich@t-online.de>
2026-01-03 11:50:49 +0100

    Bind dummy data to components

--- frontend/morning-grind/src/pages/home/sections/Offers.jsx
+++ frontend/morning-grind/src/pages/home/sections/Offers.jsx
@@ -1,44 +1,26 @@
 import Product from "../../../components/Product";
+import { products } from "../../../dummy-data/dummy-data";
 
 export default function Offers() {
-  const products = [
-    {
-      id: 1,
-      name: "Sample Product Name",
-      price: "15€",
-      color: "text-indigo-900",
-    },
-    {
-      id: 2,
-      name: "Sample Product Name",
-      price: "15€",
-      color: "text-pink-900",
-    },
-    {
-      id: 3,
-      name: "Sample Product Name",
-      price: "15€",
-      color: "text-cyan-900",
-    },
-  ];
-
   return (
     <section className="px-8 py-20">
       <div className="flex gap-12">
         <div className="flex-1">
           <h4 className="mb-6 text-3xl">Special Offers</h4>
-          <p className="font-body mb-4 text-gray-700">
+          <p className="font-body mb-4 text-gray-500">
             Discover our limited-time coffee deals.
           </p>
-          <p className="font-body text-gray-700">
+          <p className="font-body text-gray-500">
             Grab your favorites before they're gone!
           </p>
         </div>
 
-        <div className="grid flex-2 grid-cols-3 gap-16">
-          {products.map((product) => (
-            <Product product={product} />
-          ))}
+        <div className="grid flex-2 grid-cols-3 gap-8">
+          {products.map((product) =>
+            product.isOffer ? (
+              <Product key={product.id} product={product} />
+            ) : null,
+          )}
         </div>
       </div>
     </section>
