57ffd3e562e848258a665770220b1470cc086257
Lukas5937 <lukas-reich@t-online.de>
2026-01-04 09:40:04 +0100

    Add scroll restauration to App.jsx

--- frontend/morning-grind/src/App.jsx
+++ frontend/morning-grind/src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
-import { Outlet } from "react-router";
+import { Outlet, ScrollRestoration } from "react-router";
 
 function App() {
   return (
     <>
+      <ScrollRestoration />
       <main>
         <Outlet />
       </main>
