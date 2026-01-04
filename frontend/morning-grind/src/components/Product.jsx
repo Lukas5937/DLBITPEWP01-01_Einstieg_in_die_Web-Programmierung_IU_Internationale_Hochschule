bfbf6c64e9dc2532f21576986dcf4ec6fca25151
Lukas5937 <lukas-reich@t-online.de>
2026-01-04 11:52:47 +0100

    Move productColorMap to ui-helpers.js file and use it in Product component and ProductDetail page

--- frontend/morning-grind/src/components/Product.jsx
+++ frontend/morning-grind/src/components/Product.jsx
@@ -1,26 +1,6 @@
+import { productColorMap } from "../helpers/ui-helpers";
+
 export default function Product({ product }) {
-  const productColorMap = {
-    1: "text-cyan-950",
-    2: "text-cyan-900",
-    3: "text-cyan-800",
-    4: "text-cyan-700",
-
-    5: "text-orange-950",
-    6: "text-orange-900",
-    7: "text-orange-800",
-    8: "text-orange-700",
-
-    9: "text-indigo-950",
-    10: "text-indigo-900",
-    11: "text-indigo-800",
-    12: "text-indigo-700",
-
-    13: "text-rose-950",
-    14: "text-rose-900",
-    15: "text-rose-800",
-    16: "text-rose-700",
-  };
-
   return (
     <div
       key={product.id}
