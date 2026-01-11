fdbbc77d05d498d44caac22c3060206e7f04ff36
Lukas5937 <lukas-reich@t-online.de>
2026-01-11 09:56:59 +0100

    Add UUID to entity classes

--- backend/backend/src/main/java/de/morninggrind/backend/domain/Category.java
+++ backend/backend/src/main/java/de/morninggrind/backend/domain/Category.java
@@ -1,10 +1,11 @@
 package de.morninggrind.backend.domain;
 
 import lombok.Data;
+import java.util.UUID;
 
 @Data
 public class Category {
-    private Long id;
+    private UUID id;
     private String name;
     private String description;
     private boolean isActive;
