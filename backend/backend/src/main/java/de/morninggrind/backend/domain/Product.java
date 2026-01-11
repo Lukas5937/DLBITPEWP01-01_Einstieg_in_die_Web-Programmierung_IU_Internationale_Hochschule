0a3bd28299321bf4445750556080ee6e3ee27d79
Lukas5937 <lukas-reich@t-online.de>
2026-01-11 13:40:21 +0100

    Change prices from type float to BigDecimal

--- backend/backend/src/main/java/de/morninggrind/backend/domain/Product.java
+++ backend/backend/src/main/java/de/morninggrind/backend/domain/Product.java
@@ -1,13 +1,15 @@
 package de.morninggrind.backend.domain;
 
 import lombok.Data;
+
+import java.math.BigDecimal;
 import java.util.UUID;
 
 @Data
 public class Product {
     private UUID id;
     private String name;
-    private float price;
+    private BigDecimal price;
     private boolean isActive;
     private boolean isOffer;
     private Long categoryId;
