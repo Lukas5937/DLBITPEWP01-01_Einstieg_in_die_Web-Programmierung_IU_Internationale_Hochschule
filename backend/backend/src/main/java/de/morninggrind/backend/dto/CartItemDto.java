d4187f9ecd750eba1cb160ce14c354fffa2b7ae2
Lukas5937 <lukas-reich@t-online.de>
2026-01-17 09:02:17 +0100

    Create DTOs für cart

---
+++ backend/backend/src/main/java/de/morninggrind/backend/dto/CartItemDto.java
@@ -0,0 +1,20 @@
+package de.morninggrind.backend.dto;
+
+import de.morninggrind.backend.domain.Category;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Data
+public class CartItemDto {
+
+    private UUID productId;
+    private String productName;
+    private Category category;
+    private String origin;
+    private String processing;
+    private BigDecimal price;
+    private int quantity;
+    private BigDecimal itemTotal;
+}
