c331dcb6dbe78e6743c6c77708ad371eaa61afb9
Lukas5937 <lukas-reich@t-online.de>
2026-01-23 12:56:09 +0100

    Implement getCurrentUser method in AuthController

--- backend/backend/src/main/java/de/morninggrind/backend/controller/AuthController.java
+++ backend/backend/src/main/java/de/morninggrind/backend/controller/AuthController.java
@@ -6,10 +6,7 @@ import de.morninggrind.backend.dto.RegistrationRequestDto;
 import de.morninggrind.backend.service.UserService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -33,5 +30,14 @@ public class AuthController {
     @PostMapping("/logout")
     public void logout(HttpServletRequest request) throws ServletException {
         request.logout();
+    }
+
+    @GetMapping("/me")
+    public User getCurrentUser(HttpServletRequest request) {
+        User user = (User) request.getSession().getAttribute("user");
+        if (user == null) {
+            throw new RuntimeException("Not logged in");
+        }
+        return user;
     }
 }
